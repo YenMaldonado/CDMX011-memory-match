9e1d231d6fff4e3443121ad1f3169465ece25e4a
Yenny Maldonado <yenyamaldonado@gmail.com>
2021-09-09 00:54:35 -0500

    imprime y duplica data

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
-import App from './components/App.js';
+//import {App} from './components/App.js';
+import data from '../data/webdev/webdev.js';
+//console.log(data);
+const setData = document.getElementById('root');
 
-document.getElementById('root').appendChild(App());
+getCards();
+
+function getCards (){
+    setData.innerHTML = "";
+    let newArr = data.items;
+    let cloneArr = newArr;
+    cloneArr = cloneArr.concat(newArr);
+    cloneArr.forEach(printCard =>{
+    let mainCards = document.createElement("div");
+    mainCards.innerHTML = `
+    <img src="${printCard.image} " height= "120px"; width= "100px;"/>
+    `;
+    setData.appendChild(mainCards); 
+     //console.log(showCards);
+     return printCard;
+    })
+    
+}
+
